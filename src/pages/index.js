944254917e3e0da3f4d6786fc50478eae055149a
Zublevska <83469396+Zublevska@users.noreply.github.com>
2021-07-21 23:44:32 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 const { AuthPage } = require('./auth.page');
+const { ProfilePage } = require('./profile.page');
+const { SpecialtyclinicPage } = require('./specialtyclinic.page');
+const { AddClinicPage } = require('./addclinic.page');
 
 class App {
   constructor() {
     this.authPage = new AuthPage();
+    this.profilePage = new ProfilePage();
+    this.specialtyclinicPage = new SpecialtyclinicPage();
+    this.addclinicPage = new AddClinicPage();
   }
 }
 
